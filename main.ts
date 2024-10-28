99ad62f9bd29b7154b477fb4ff5e48382404254c
DiegoCaroFrechilla <146827294+DiegoCaroFrechilla@users.noreply.github.com>
2024-10-28 15:20:19 +0100

    ti ti ta 🎶

--- main.ts
+++ main.ts
@@ -1,6 +1,9 @@
 input.onButtonPressed(Button.A, function () {
     music.play(music.stringPlayable("C5 G B A F A C5 B ", 120), music.PlaybackMode.UntilDone)
 })
+input.onGesture(Gesture.Shake, function () {
+    music.setTempo(200)
+})
 input.onButtonPressed(Button.B, function () {
     music.play(music.stringPlayable("G C5 G A B E E A ", 100), music.PlaybackMode.UntilDone)
 })
